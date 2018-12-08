ecc177bc80817bec2934b64c86d35bbe7cc11b2f
Robert Herschel Hawk <robert@the-hawk.us>
2018-12-08 03:46:07 -0700

    Added a no alerts message to the /alerts view

--- src/components/Alert.vue
+++ src/components/Alert.vue
@@ -1,12 +1,19 @@
 <template>
   <div id="alert">
-    <h5 class="card-title">
-      {{ this.alert.issuer }} has issued a {{ this.alert.severity }}
-      {{ this.alert.event }} until {{ this.until }}.
-    </h5>
-    <div id="alertText">
-      <p>{{ this.alert.description }}</p>
-      <p>{{ this.alert.instruction }}</p>
+    <div v-if="alert.id !== '$'">
+      <h5 class="card-title">
+        {{ this.alert.issuer }} has issued a {{ this.alert.severity }}
+        {{ this.alert.event }} until {{ this.until }}.
+      </h5>
+      <div id="alertText">
+        <p>{{ this.alert.description }}</p>
+        <p>{{ this.alert.instruction }}</p>
+      </div>
+    </div>
+    <div v-else>
+      <h5 class="card-title">
+        No weather alerts have been issued for your area.
+      </h5>
     </div>
   </div>
 </template>
--- src/views/Alerts.vue
+++ src/views/Alerts.vue
@@ -1,12 +1,22 @@
 <template>
   <div id="alerts">
-    <div v-if="alerts">
+    <div v-if="fetched && alerts.length !== 0">
       <div v-for="alert in alerts" :key="alert.id" class="row">
         <div class="col-sm-1"></div>
         <Alert
           :alert="alert"
           class="card col-sm-10"
         />
+      </div>
+    </div>
+    <div v-else-if="fetched && alerts.length === 0">
+      <div class="row">
+        <div class="col-sm-1"></div>
+        <div class="card col-sm-10">
+          <h5 class="card-title">
+            No alerts have been issued for your area.
+          </h5>
+        </div>
       </div>
     </div>
     <div v-else>
@@ -28,12 +38,14 @@ export default {
   data() {
     return {
       pos: null,
+      fetched: false,
       alerts: [],
     };
   },
   methods: {
-    parseAlerts(alerts) {
-      alerts.features.forEach((raw) => {
+    parseAlerts(rawAlerts) {
+      this.fetched = true;
+      rawAlerts.features.forEach((raw) => {
         const alert = {
           id: raw.properties.id,
           issuer: raw.properties.senderName,
